cfb3773c1ee077d7d23bc26fb353f8288a9a7683
manuelDiazpas <diazpastormanuel0@gmail.com>
2024-10-03 20:08:47 +0200

    Finalización del ejercicio 21

--- Ej21/commons.js
+++ Ej21/commons.js
@@ -1,6 +1,6 @@
 /*21. Crear una página index.html que contenga los siguientes botones:
-◦ Botón que abre dos ventanas: ventana1.html y ventana2.html, ambas de 300x200. La ventana1 en
-la posición 100,300 y la ventana2 en la posición 500,300
+◦ Botón que abre dos ventanas: ventana1.html y ventana2.html, ambas de 300x200. 
+La ventana1 en la posición 100,300 y la ventana2 en la posición 500,300
 ◦ Botón que cierra las dos ventanas
 ◦ Botón para cambiar el fondo de la primera ventana a:#FFECA1
 ◦ Botón para cambiar el fondo de la segunda ventana a:#EFC3CA
@@ -13,48 +13,51 @@ ventana2.html tiene dos botones:
 Incluir todo el código JavaScript en un único fichero accion.js
 */
 
-var nuevaVentana;
+var COLOR_VENTANA1 = "#FFECA1";
+var COLOR_VENTANA2 = "#EFC3CA";
+
+var primeraVentana;
+var segundaVentana;
 
 function boton1() {
-  nuevaVentana = window.open(
-    "index_secundario.html",
-    "Pagina Secundaria",
-    "width=300, height=400, top=400, left=400"
+  primeraVentana = window.open(
+    "ventana1.html",
+    "Primera Ventana",
+    "width=300, height=200, top=100, left=300"
+  );
+
+  segundaVentana = window.open(
+    "ventana2.html",
+    "Segunda Ventana",
+    "width=300, height=200, top=500, left=300"
   );
 }
 
 function boton2() {
-  nuevaVentana.focus();
-  nuevaVentana.moveBy(200, 100);
+  primeraVentana.close();
+  segundaVentana.close();
 }
 
 function boton3() {
-  nuevaVentana.focus();
-  nuevaVentana.moveTo(500, 200);
+  primeraVentana.document.body.style.backgroundColor = COLOR_VENTANA1;
 }
 
 function boton4() {
-  nuevaVentana.focus();
-  nuevaVentana.resizeBy(100, 100);
-}
-
-function boton5() {
-  nuevaVentana.focus();
-  nuevaVentana.resizeTo(100, 150);
+  segundaVentana.document.body.style.backgroundColor = COLOR_VENTANA2;
 }
 
 function boton1_1() {
-  window.document.body.style.backgroundColor = "Green";
+  opener.segundaVentana.alert("La ventana 1 te saluda");
 }
 
 function boton1_2() {
-  window.opener.document.body.style.backgroundColor = "Red";
+  opener.segundaVentana.document.body.style.backgroundColor = "#7DDA58";
 }
 
-function boton1_3() {
-  window.close();
+function boton2_1() {
+  opener.primeraVentana.alert("La ventana 2 te saluda");
 }
 
-function boton1_4() {
-  window.opener.close();
+function boton2_2() {
+  opener.primeraVentana.document.body.style.backgroundColor = "#5DE2E7";
 }
